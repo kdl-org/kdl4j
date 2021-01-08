a6ef9896e009d5837f624a946ffee9117a8e3d9f
Cory <cory.kolbeck@streem.pro>
2021-01-08 08:21:43 -0800

    Tweak grammar a bit more, print strings appropriately quoted

--- src/main/java/dev/cbeck/kdl/objects/PrintUtil.java
+++ src/main/java/dev/cbeck/kdl/objects/PrintUtil.java
@@ -2,15 +2,46 @@ package dev.cbeck.kdl.objects;
 
 import java.io.IOException;
 import java.io.Writer;
+import java.util.function.Predicate;
+import java.util.regex.Pattern;
 
 class PrintUtil {
+    private static final Predicate<String> VALID_BARE_ID = Pattern.compile(
+            "[^\n\r\u000C\u0085\u2028\u2029{}<>;\\[\\]=,\"\u0009\u0020\u00A0\u1680\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200A\u202F\u205F\u3000]" +
+            "[^\n\r\u000C\u0085\u2028\u2029{}<>;\\[\\]=,\"\u0009\u0020\u00A0\u1680\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200A\u202F\u205F\u30000-9]*"
+    ).asPredicate();
+    
     static void writeStringQuotedAppropriately(Writer writer, String string, boolean bareAllowed) throws IOException {
-        if (!bareAllowed) {
-            writer.write('"');
+        int quoteAt = string.indexOf('"');
+        int hashDepth = 0;
+        while (quoteAt >= 0) {
+            int hashes = 0;
+            for (int i = quoteAt + 1; i < string.length() && string.charAt(i) == '#'; i++) {
+                hashes++;
+            }
+            hashDepth = Math.max(hashDepth, hashes);
+            quoteAt = string.indexOf('"', quoteAt + 1);
         }
-        writer.write(string);
-        if (!bareAllowed) {
+
+        if (hashDepth == 0) {
+            if (bareAllowed && VALID_BARE_ID.test(string)) {
+                writer.write(string);
+            } else {
+                writer.write('"');
+                writer.write(string);
+                writer.write('"');
+            }
+        } else {
+            writer.write('r');
+            for (int i = 0; i <= hashDepth; i++) {
+                writer.write('#');
+            }
             writer.write('"');
+            writer.write(string);
+            writer.write('"');
+            for (int i = 0; i <= hashDepth; i++) {
+                writer.write('#');
+            }
         }
     }
 }
