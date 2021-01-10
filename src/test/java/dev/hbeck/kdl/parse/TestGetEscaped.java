a9386002437696010a419ea7373a1833d961424f
Daniel Smith <jellymann@gmail.com>
2021-01-10 22:40:36 +0200

    fix: unicode escapes

--- src/test/java/dev/hbeck/kdl/parse/TestGetEscaped.java
+++ src/test/java/dev/hbeck/kdl/parse/TestGetEscaped.java
@@ -15,7 +15,7 @@ import static org.junit.Assert.assertThat;
 
 @RunWith(Parameterized.class)
 public class TestGetEscaped {
-    public TestGetEscaped(String input, int expectedResult) {
+    public TestGetEscaped(String input, String expectedResult) {
         this.input = input;
         this.expectedResult = expectedResult;
     }
@@ -23,32 +23,32 @@ public class TestGetEscaped {
     @Parameterized.Parameters(name = "{0}")
     public static List<Object[]> getCases() {
         return Stream.of(
-                new Object[]{"n", '\n'},
-                new Object[]{"r", '\r'},
-                new Object[]{"t", '\t'},
-                new Object[]{"\\", '\\'},
-                new Object[]{"\"", '\"'},
-                new Object[]{"b", '\b'},
-                new Object[]{"f", '\f'},
-                new Object[]{"u{1}", '\u0001'},
-                new Object[]{"u{01}", '\u0001'},
-                new Object[]{"u{001}", '\u0001'},
-                new Object[]{"u{001}", '\u0001'},
-                new Object[]{"u{0001}", '\u0001'},
-                new Object[]{"u{00001}", '\u0001'},
-                new Object[]{"u{000001}", '\u0001'},
-                new Object[]{"ux", -2},
-                new Object[]{"u{x}", -2},
-                new Object[]{"u{0001", -2},
-                new Object[]{"u{AX}", -2},
-                new Object[]{"u{}", -2},
-                new Object[]{"u{0000001}", -2},
-                new Object[]{"u{11FFF}", -2}
+                new Object[]{"n", "\n"},
+                new Object[]{"r", "\r"},
+                new Object[]{"t", "\t"},
+                new Object[]{"\\", "\\"},
+                new Object[]{"\"", "\""},
+                new Object[]{"b", "\b"},
+                new Object[]{"f", "\f"},
+                new Object[]{"u{1}", "\u0001"},
+                new Object[]{"u{01}", "\u0001"},
+                new Object[]{"u{001}", "\u0001"},
+                new Object[]{"u{001}", "\u0001"},
+                new Object[]{"u{0001}", "\u0001"},
+                new Object[]{"u{00001}", "\u0001"},
+                new Object[]{"u{000001}", "\u0001"},
+                new Object[]{"ux", ""},
+                new Object[]{"u{x}", ""},
+                new Object[]{"u{0001", ""},
+                new Object[]{"u{AX}", ""},
+                new Object[]{"u{}", ""},
+                new Object[]{"u{0000001}", ""},
+                new Object[]{"u{110000}", ""}
         ).collect(Collectors.toList());
     }
 
     private final String input;
-    private final int expectedResult;
+    private final String expectedResult;
 
     @Test
     public void doTest() throws IOException {
@@ -56,10 +56,10 @@ public class TestGetEscaped {
         final int initial = context.read();
 
         try {
-            final int result = TestUtil.parser.getEscaped(initial, context);
+            final String result = TestUtil.parser.getEscaped(initial, context);
             assertThat(result, equalTo(expectedResult));
         } catch (KDLParseException e) {
-            if (expectedResult > 0) {
+            if (expectedResult != "") {
                 throw new KDLParseException("Expected no errors", e);
             }
         }
