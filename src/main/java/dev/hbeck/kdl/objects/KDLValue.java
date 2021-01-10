d1e8a9ad9ecbebd176cdef7486459a92e6bb588b
Daniel Smith <jellymann@gmail.com>
2021-01-10 22:37:37 +0200

    add: generic KDLValue.from

--- src/main/java/dev/hbeck/kdl/objects/KDLValue.java
+++ src/main/java/dev/hbeck/kdl/objects/KDLValue.java
@@ -1,6 +1,8 @@
 package dev.hbeck.kdl.objects;
 
 import java.util.Optional;
+import java.math.BigDecimal;
+import java.math.BigInteger;
 
 public interface KDLValue extends KDLObject {
     KDLString getAsString();
@@ -23,5 +25,28 @@ public interface KDLValue extends KDLObject {
 
     default boolean isNull() {
         return false;
+    }
+
+    static KDLValue from(Object o) {
+        if (o == null) return KDLNull.INSTANCE;
+        if (o instanceof Boolean) {
+            if ((Boolean) o) return KDLBoolean.TRUE;
+            return KDLBoolean.FALSE;
+        }
+        if (o instanceof BigInteger) {
+            return new KDLNumber(new BigDecimal((BigInteger)o), 10);
+        }
+        if (o instanceof BigDecimal) {
+            return new KDLNumber((BigDecimal)o, 10);
+        }
+        if (o instanceof Number) {
+            return new KDLNumber(new BigDecimal(o.toString()), 10);
+        }
+        if (o instanceof String) {
+            return new KDLString((String)o);
+        }
+        if (o instanceof KDLValue) return (KDLValue)o;
+
+        throw new RuntimeException("No KDLValue for object " + o.toString());
     }
 }
