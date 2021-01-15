015cd3ce47babacde6520940e4a6352c1ca040df
Cory <cory.kolbeck@streem.pro>
2021-01-15 11:57:06 -0800

    More mutation parsing

--- src/main/java/dev/hbeck/kdl/objects/KDLNode.java
+++ src/main/java/dev/hbeck/kdl/objects/KDLNode.java
@@ -332,6 +332,11 @@ public class KDLNode implements KDLObject {
             return this;
         }
 
+        public Builder clearArgs() {
+            args.clear();
+            return this;
+        }
+
         public KDLNode build() {
             Objects.requireNonNull(identifier, "Identifier must be set");
 
