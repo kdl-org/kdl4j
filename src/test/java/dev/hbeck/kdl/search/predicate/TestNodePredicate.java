422a1242120060864d31a36355279e2a5c272340
Cory <cory.kolbeck@streem.pro>
2021-01-17 20:24:02 -0800

    Finish testing of predicates

--- src/test/java/dev/hbeck/kdl/search/predicate/TestNodePredicate.java
+++ src/test/java/dev/hbeck/kdl/search/predicate/TestNodePredicate.java
@@ -1,4 +1,52 @@
 package dev.hbeck.kdl.search.predicate;
 
+import dev.hbeck.kdl.objects.KDLNode;
+import dev.hbeck.kdl.search.predicates.NodeContentPredicate;
+import dev.hbeck.kdl.search.predicates.NodePredicate;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.function.Predicate;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
 public class TestNodePredicate {
+
+    @Mock
+    public Predicate<String> idPredicate;
+
+    @Mock
+    public NodeContentPredicate contentPredicate;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    public void test() {
+        final NodePredicate predicate = new NodePredicate(idPredicate, contentPredicate);
+        final KDLNode node = KDLNode.builder().setIdentifier("identifier").build();
+
+        when(idPredicate.test(any())).thenReturn(false);
+        when(contentPredicate.test(any())).thenReturn(false);
+        assertFalse(predicate.test(node));
+
+        when(idPredicate.test(any())).thenReturn(true);
+        when(contentPredicate.test(any())).thenReturn(false);
+        assertFalse(predicate.test(node));
+
+        when(idPredicate.test(any())).thenReturn(false);
+        when(contentPredicate.test(any())).thenReturn(true);
+        assertFalse(predicate.test(node));
+
+        when(idPredicate.test(any())).thenReturn(true);
+        when(contentPredicate.test(any())).thenReturn(true);
+        assertTrue(predicate.test(node));
+    }
 }
--- src/test/java/dev/hbeck/kdl/search/predicate/TestPropPredicate.java
+++ src/test/java/dev/hbeck/kdl/search/predicate/TestPropPredicate.java
@@ -1,4 +1,53 @@
 package dev.hbeck.kdl.search.predicate;
 
+import dev.hbeck.kdl.objects.KDLNode;
+import dev.hbeck.kdl.objects.KDLValue;
+import dev.hbeck.kdl.search.predicates.PropPredicate;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.function.Predicate;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
 public class TestPropPredicate {
+    @Mock
+    public Predicate<String> keyPredicate;
+
+    @Mock
+    public Predicate<KDLValue> valuePredicate;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    public void test() {
+        final PropPredicate predicate = new PropPredicate(keyPredicate, valuePredicate);
+        final KDLNode node = KDLNode.builder().setIdentifier("identifier")
+                .addProp("key", "val")
+                .build();
+
+        when(keyPredicate.test(any())).thenReturn(false);
+        when(valuePredicate.test(any())).thenReturn(false);
+        assertFalse(predicate.test(node));
+
+        when(keyPredicate.test(any())).thenReturn(true);
+        when(valuePredicate.test(any())).thenReturn(false);
+        assertFalse(predicate.test(node));
+
+        when(keyPredicate.test(any())).thenReturn(false);
+        when(valuePredicate.test(any())).thenReturn(true);
+        assertFalse(predicate.test(node));
+
+        when(keyPredicate.test(any())).thenReturn(true);
+        when(valuePredicate.test(any())).thenReturn(true);
+        assertTrue(predicate.test(node));
+    }
 }
