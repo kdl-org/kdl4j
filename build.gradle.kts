e039fd16d21ac495762f4caba7e5083dff2f2fc3
Cory <cory.kolbeck@streem.pro>
2021-01-20 11:50:32 -0800

    Fix gradle build

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,3 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
     java
     jacoco
@@ -43,15 +41,4 @@ tasks.jacocoTestReport {
 dependencies {
     testImplementation("junit", "junit", "4.12")
     testImplementation("org.mockito", "mockito-core", "3.7.7")
-    implementation(kotlin("stdlib-jdk8"))
-}
-
-val compileKotlin: KotlinCompile by tasks
-compileKotlin.kotlinOptions {
-    jvmTarget = "1.8"
-}
-
-val compileTestKotlin: KotlinCompile by tasks
-compileTestKotlin.kotlinOptions {
-    jvmTarget = "1.8"
 }
